21b1b05d9974159be940694d90be470b942f3420
blandineruth <ayiiteruth004@gmail.com>
2023-11-29 07:16:10 +0000

    first commit

---
+++ movies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=225)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=225)
+    release_year = models.ImageField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    Genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now())
