7b96f6297421d8e272f6234e64525f36d4e97f60
blandineruth <ayiiteruth004@gmail.com>
2023-11-30 05:13:18 +0000

    un ajout du code

--- movies/models.py
+++ movies/models.py
@@ -8,11 +8,14 @@ from django.utils import timezone
 class Genre(models.Model):
     name = models.CharField(max_length=225)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=225)
-    release_year = models.ImageField()
+    release_year = models.IntegerField()
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     Genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(default=timezone.now())
+    date_created = models.DateTimeField(default=timezone.now)
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,16 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+# output = ', '.join([m.title for m in movies]) affichage 
+# par url  des title de movies  
+# return HttpResponse(output)
 
