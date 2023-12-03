90277296f923f2d3931e3f1a7abe26208b745079
blandineruth <ayiiteruth004@gmail.com>
2023-12-03 01:06:23 +0000

    correction du projet

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .import views
 
 
+app_name = 'movies'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
